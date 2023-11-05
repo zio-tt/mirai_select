0600d600379e275d671c6e0c3f1a9178bde14198
zio-tt <zio.tt.dev@gmail.com>
2023-11-05 14:24:07 +0900

    setting cors

--- api/config/initializers/omniauth.rb
+++ api/config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            Rails.application.credentials.google[:client_id],
-           Rails.application.credentials.google[:client_secret]
+           Rails.application.credentials.google[:client_secret],
+           {
+             scope: 'userinfo.email,userinfo.profile',
+             prompt: 'select_account',
+           }
 end
