08edc2a9689b085cb16a0c4866c7ad8c99eb9931
zio-tt <zio.tt.dev@gmail.com>
2023-11-10 23:56:52 +0900

    restored Rails and configured Git.

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def create
+    user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])                      
+    if user
+      head :ok
+    else
+      render json: { error: "ログインに失敗しました" }, status: :unprocessable_entity
+    end
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
+  end
 end
