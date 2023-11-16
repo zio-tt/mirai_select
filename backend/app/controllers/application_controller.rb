d2f09d2d7479d13958e2b55115bb25729e7b037b
zio-tt <zio.tt.dev@gmail.com>
2023-11-16 14:51:23 +0900

    Feature: Install Pundit

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   include Authentication
+  rescue_from Pundit::NotAuthorizedError, with: :render_403
   before_action :check_xhr_header
   before_action :current_user
 
   def check_xhr_header
     return if request.xhr?
     render json: { error: 'forbidden' }, status: :forbidden
+  end
+
+  def render_403
+    render status: 403, json: { message: "You don't have permission." }
   end
 end
