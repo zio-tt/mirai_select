7f8947061f6c6bf6c0cbd73f962ed716fe0911d2
zio-tt <zio.tt.dev@gmail.com>
2023-11-20 21:36:20 +0900

    WIP feature: helper

--- frontend/app/(routes)/(private)/helper/page.tsx
+++ frontend/app/(routes)/(private)/helper/page.tsx
@@ -1,37 +1,87 @@
 "use client";
 
+import  "./style.css";
 import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { MouseEvent } from "react";
 
-export default function decisionHelper () {
+export default function decisionHelperFirstInput () {
   const [ inputText, setInputText ] = useState<string>('');
   const [ remainingChars, setRemainingChars ] = useState<number>(50);
+  const [ isLoading , setIsLoading ] = useState<boolean>(false);
+  const [ alertFlag , setAlertFlag ] = useState<boolean>(false);
   const maxChars = 50;
 
   useEffect(() => {
     setRemainingChars(maxChars - inputText.length);
+    if (inputText.length > maxChars) {
+      setAlertFlag(true);
+    } else {
+      setAlertFlag(false);
+    }
+    if (inputText.length > 0) {
+      sessionStorage.setItem('firstHelperInputText', inputText);
+    }
   }, [inputText]);
+
+  useEffect(() => {
+    const storedInputText = sessionStorage.getItem('firstHelperInputText');
+    if (storedInputText) {
+      setInputText(storedInputText);
+    }
+  }, []);
+
+
+  function sendText(MouseEvent: MouseEvent<HTMLElement>) {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  }
 
   return (
     <>
-      {/* 機能説明画面 */}
-      {/* 入力画面 */}
-      <div className="w-screen flex items-center justify-center">
-        <div className="form-control w-full max-w-3xl">
-          <input
-            type="text"
-            placeholder="悩み事を入力してください（最大50文字）"
-            id="input_text"
-            className="input input-bordered w-full max-w-full"
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-          />
-          <label
-            className="label text-sm text-base-100"
-            id="char_counter"
-            style={{ color: remainingChars < 0 ? 'red' : 'black' }}
-          >
-            残り{remainingChars}文字
-          </label>
+    {/* 機能説明画面 */}
+    {/* 入力画面 */}
+      <div className="w-screen flex flex-col items-center justify-center">
+      {/* タイトル */}
+        <div className="text-3xl">悩みごと</div>
+      {/* 警告文 */}
+        { alertFlag && ( <div className="text-xl" style={{color: 'red'}}>文字数がオーバーしています。入力する悩みごとは<span className="underline">50文字以内</span>にしてください。</div> )}
+      {/* 入力された文字列 */}
+        <div className="text-window w-[60%] h-[10%]  mt-2 mb-8 relative py-2 px-6 border-t-2 border-b-2 border-black flex items-center justify-center"><p className="text-xl">{ inputText }</p></div>
+      {/* 入力フォーム */}
+        <div className="flex flex-row w-screen items-center justify-center">
+          <div className="form-control w-full max-w-lg">
+            <input
+              type="text"
+              placeholder="悩みごとを入力してください（最大50文字）"
+              id="input_text"
+              className="input input-bordered border-black w-full max-w-full"
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+            />
+            <label
+              className="label text-sm text-base-100"
+              id="char_counter"
+              style={{ color: remainingChars < 0 ? 'red' : 'black' }}
+            >
+              残り{remainingChars}文字
+            </label>
+          </div>
+        {/* 入力後0.5秒程度ローディングボタン */}
+          { remainingChars >= 0 && (
+            <div className="h-full items-start justify-start ml-4">
+              { !isLoading && ( <Link href="#" onClick={sendText}><button className="btn btn-outline">Send</button></Link> )}
+              { isLoading && (
+                <button className="btn btn-outline">
+                  <span className="loading loading-spinner"></span>
+                  loading
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </div>
       {/* 結果画面 */}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,11 +8,11 @@ import { GoogleLoginButton} from '@/app/_components/ui-elements';
 import { logger } from '@/app/_common/utils/logger'
 
 export default function Home() {
-  const [showLogo, setShowLogo] = useState(true);
-  const [showButton, setShowButton] = useState(false);
-  const [logoOpacity, setLogoOpacity] = useState(0);
-  const [buttonOpacity, setButtonOpacity] = useState(0);
-  const [showContent , setShowContent ] = useState(false);
+  const [ showLogo, setShowLogo] = useState(true);
+  const [ showButton, setShowButton] = useState(false);
+  const [ logoOpacity, setLogoOpacity] = useState(0);
+  const [ buttonOpacity, setButtonOpacity] = useState(0);
+  const [ showContent , setShowContent ] = useState(false);
   const { data: session, status } = useSession();
   const router = useRouter();
 
@@ -43,39 +43,40 @@ export default function Home() {
     };
   }
 
-  const [ imageURL, setImageURL ] = React.useState<string>("/images/top/sample1.jpeg");
+  const [ helperImageURL, setHelperImageURL ] = React.useState<string>("/images/top/sample1.jpeg");
+  const [ indexImageURL, setIndexImageURL ] = React.useState<string>("/images/top/sample1.jpeg");
+  const [ helperText, setHelperText ] = React.useState<string>("1. あなたの悩みごとを入力");
+  const [ indexText, setIndexText ] = React.useState<string>("1. あなたの悩みごとを入力");
 
   const switchHelper = (event: MouseEvent<HTMLElement>) => {
     event.preventDefault();
     const key = event.currentTarget.getAttribute('data-key');
-    const helperText = document.querySelector('.helperText');
 
-    if (key === "index-talk") {
-      setImageURL("/images/top/sample1.jpeg");
-      helperText!.innerHTML = "1. あなたの悩みごとを入力";
-    } else if (key === "index-discussion") {
-      setImageURL("/images/top/sample2.webp");
-      helperText!.innerHTML = "2. AIが悩みごとを分析";
-    } else if (key === "index-happy") {
-      setImageURL("/images/top/sample3.jpeg");
-      helperText!.innerHTML = "3. あなたの決断をサポート";
+    if (key === "helper-talk") {
+      setHelperImageURL("/images/top/sample1.jpeg");
+      setHelperText("1. あなたの悩みごとを入力");
+    } else if (key === "helper-discussion") {
+      setHelperImageURL("/images/top/sample2.webp");
+      setHelperText("2. AIが悩みごとを分析");
+    } else if (key === "helper-happy") {
+      setHelperImageURL("/images/top/sample3.jpeg");
+      setHelperText("3. あなたの決断をサポート");
     }
   };
 
-  const handleClick = (event: MouseEvent<HTMLElement>) => {
+  const switchIndex = (event: MouseEvent<HTMLElement>) => {
     event.preventDefault();
     const key = event.currentTarget.getAttribute('data-key');
-    const helperText = document.querySelector('.helperText');
 
-    if (key === "helper-talk") {
-      setImageURL("/images/top/sample1.jpeg");
-      helperText!.innerHTML = "1. あなたの悩みごとを入力";
-    } else if (key === "helper-discussion") {
-      setImageURL("/images/top/sample2.webp");
-      helperText!.innerHTML = "2. AIが悩みごとを分析";
-    } else if (key === "helper-happy") {
-      setImageURL("/images/top/sample3.jpeg");
-      helperText!.innerHTML = "3. あなたの決断をサポート";
+    if (key === "index-talk") {
+      setIndexImageURL("/images/top/sample1.jpeg");
+      setIndexText("1. あなたの悩みごとを入力");
+    } else if (key === "index-discussion") {
+      setIndexImageURL("/images/top/sample2.webp");
+      setIndexText("2. AIが悩みごとを分析");
+    } else if (key === "index-happy") {
+      setIndexImageURL("/images/top/sample3.jpeg");
+      setIndexText("3. あなたの決断をサポート");
     }
   };
 
@@ -99,92 +100,71 @@ export default function Home() {
 
   return (
     <>
-      { !showContent && (
-        <div className="flex flex-col items-center justify-center">
-          <div
-            className={'transform transition-opacity duration-1000'}
-            style={{ display: showLogo ? 'block' : 'none', opacity: logoOpacity }}
-          >
-            {showLogo && (
-              <Image src="/images/logo.png" alt="Logo" width={500} height={500} style = {{ objectFit: "contain" }} />
-            )}
+      <div className="flex flex-col items-center justify-start pt-16 min-h-screen w-screen bg-white">
+        <div className="w-[80%] flex flex-col items-center justify-center">
+          {/* 1段目のコンテンツ */}
+          <div className="flex flex-row items-center justify-center mb-10"> {/* mb-10 は下のマージン */}
+            {/* 左側のテキストとボタン */}
+            <div className="flex flex-col items-center justify-center mr-20">
+              <div className="text-center min-w-[50%] lg:text-left">
+                <h1 className="text-2xl font-bold underline overflow-hidden"><span className="text-3xl text-blue-500">決断ヘルパー機能</span>であなたの決断をサポート</h1>
+                <div className="py-6 text-bs font-bold overflow-hidden">
+                  <p>2人のキャラクターがそれぞれの視点に立ってあなたの悩みごとに対する選択肢を提示します。</p>
+                  <p>会話にはタグをつけて、「みんなの悩みごと」で公開することができます。</p>
+                  <p>今後、キャラクターカスタマイズ機能を追加予定！</p>
+                </div>
+                <p>各アイコンをクリックして画像を表示</p>
+                <div className="flex w-[80%] p-3 border-2 border-black rounded-lg items-center justify-center">
+                  <a href="#" onClick={switchHelper} data-key="helper-talk"><Image src="/images/talk.png" alt="Talk" width={100} height={100} /></a>
+                  <Image src="/images/arrow.png" alt="Arrow" width={80} height={80} />
+                  <a href="#" onClick={switchHelper} data-key="helper-discussion"><div>
+                    <Image src="/images/discussion.png" alt="Discussion" width={80} height={80} />
+                    <Image src="/images/pc.png" alt="PC" width={80} height={80} />
+                  </div></a>
+                  <Image src="/images/arrow.png" alt="Arrow" width={80} height={80} />
+                  <a href="#" onClick={switchHelper} data-key="helper-happy"><Image src="/images/happy.png" alt="Happy" width={100} height={100} /></a>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div className="relative max-w-[100%] w-[500px] h-[400px]">
+                <Image src={helperImageURL} className="border-2 border-gray-600" alt="Image" layout="fill" />
+              </div>
+              <div className="underline text-lg text-center mt-3">{helperText}</div>
+            </div>
           </div>
-          { showButton && (
-            <div
-              className="transition-opacity duration-1000"
-              style={{ display: showButton ? 'block' : 'none', opacity: buttonOpacity }}>
-              <GoogleLoginButton />
+          <div className="flex flex-row items-center justify-center mt-10 mb-20">
+            <div className="flex flex-col mr-20">
+              <div className="relative max-w-[100%] w-[500px] h-[400px]">
+                <Image src={indexImageURL} className="border-2 border-gray-600 max-w-[100%] h-auto" alt="Image" layout="fill" />
+              </div>
+              <div className="underline text-lg text-center mt-3">{indexText}</div>
             </div>
-          )}
-        </div>
-      )}
-      { showContent && (
-        <div className="flex flex-col items-center justify-start pt-16 min-h-screen w-screen bg-white">
-          <div className="w-[80%] flex flex-col items-center justify-center">
-            {/* 1段目のコンテンツ */}
-            <div className="flex flex-row items-center justify-center mb-10"> {/* mb-10 は下のマージン */}
-              {/* 左側のテキストとボタン */}
-              <div className="flex flex-col items-center justify-center mr-20">
-                <div className="text-center min-w-[50%] lg:text-left">
-                  <h1 className="text-2xl font-bold underline overflow-hidden"><span className="text-3xl text-blue-500">決断ヘルパー機能</span>であなたの決断をサポート</h1>
-                  <div className="py-6 text-bs font-bold overflow-hidden">
-                    <p>2人のキャラクターがそれぞれの視点に立ってあなたの悩みごとに対する選択肢を提示します。</p>
-                    <p>会話にはタグをつけて、「みんなの悩みごと」で公開することができます。</p>
-                    <p>今後、キャラクターカスタマイズ機能を追加予定！</p>
-                  </div>
-                  <p>各アイコンをクリックして画像を表示</p>
-                  <div className="flex w-[80%] p-3 border-2 border-black rounded-lg items-center justify-center">
-                    <a href="#" onClick={switchHelper} data-key="helper-talk"><Image src="/images/talk.png" alt="Talk" width={100} height={100} /></a>
-                    <Image src="/images/arrow.png" alt="Arrow" width={80} height={80} />
-                    <a href="#" onClick={switchHelper} data-key="helper-discussion"><div>
-                      <Image src="/images/discussion.png" alt="Discussion" width={80} height={80} />
-                      <Image src="/images/pc.png" alt="PC" width={80} height={80} />
-                    </div></a>
-                    <Image src="/images/arrow.png" alt="Arrow" width={80} height={80} />
-                    <a href="#" onClick={switchHelper} data-key="helper-happy"><Image src="/images/happy.png" alt="Happy" width={100} height={100} /></a>
-                  </div>
+            {/* ここからテキストと画像を右寄せにしたい */}
+            <div className="flex flex-col items-center justify-center">
+              <div className="text-center min-w-[50%] lg:text-left">
+                <h1 className="text-2xl font-bold underline overflow-hidden"><span className="text-3xl text-green-500">みんなの悩みごと</span>でさまざまな事例を共有</h1>
+                <div className="py-6 text-bs font-bold overflow-hidden">
+                  <p>2人のキャラクターがそれぞれの視点に立ってあなたの悩みごとに対する選択肢を提示します。</p>
+                  <p>会話にはタグをつけて、「みんなの悩みごと」で公開することができます。</p>
+                  <p>今後、キャラクターカスタマイズ機能を追加予定！</p>
                 </div>
-              </div>
-              <div className="flex flex-col">
-                <div className="relative max-w-[100%] w-[500px] h-[400px]">
-                  <Image src={imageURL} className="border-2 border-gray-600" alt="Image" layout="fill" />
-                </div>
-                <div className="helperText underline text-lg text-center mt-3">1. あなたの悩み事を入力</div>
-              </div>
-            </div>
-            <div className="flex flex-row items-center justify-center mt-10 mb-20">
-              <div className="flex flex-col mr-20">
-                <div className="relative max-w-[100%] w-[500px] h-[400px]">
-                  <Image src={imageURL} className="border-2 border-gray-600" alt="Image" layout="fill" />
-                </div>
-                <div className="helperText underline text-lg text-center mt-3">1. あなたの悩み事を入力</div>
-              </div>
-              {/* ここからテキストと画像を右寄せにしたい */}
-              <div className="flex flex-col items-center justify-center">
-                <div className="text-center min-w-[50%] lg:text-left">
-                  <h1 className="text-2xl font-bold underline overflow-hidden"><span className="text-3xl text-green-500">みんなの悩みごと</span>でさまざまな事例を共有</h1>
-                  <div className="py-6 text-bs font-bold overflow-hidden">
-                    <p>2人のキャラクターがそれぞれの視点に立ってあなたの悩みごとに対する選択肢を提示します。</p>
-                    <p>会話にはタグをつけて、「みんなの悩みごと」で公開することができます。</p>
-                    <p>今後、キャラクターカスタマイズ機能を追加予定！</p>
-                  </div>
-                  <p>各アイコンをクリックして画像を表示</p>
-                  <div className="flex w-[80%] p-3 border-2 border-black rounded-lg items-center justify-center">
-                    <a href="#" onClick={handleClick} data-key="index-talk"><Image src="/images/talk.png" alt="Talk" width={100} height={100} /></a>
-                    <Image src="/images/arrow.png" alt="Arrow" width={80} height={80} />
-                    <a href="#" onClick={handleClick} data-key="index-discussion"><div>
-                      <Image src="/images/discussion.png" alt="Discussion" width={80} height={80} />
-                      <Image src="/images/pc.png" alt="PC" width={80} height={80} />
-                    </div></a>
-                    <Image src="/images/arrow.png" alt="Arrow" width={80} height={80} />
-                    <a href="#" onClick={handleClick} data-key="index-happy"><Image src="/images/happy.png" alt="Happy" width={100} height={100} /></a>
-                  </div>
+                <p>各アイコンをクリックして画像を表示</p>
+                <div className="flex w-[80%] p-3 border-2 border-black rounded-lg items-center justify-center">
+                  <a href="#" onClick={switchIndex} data-key="index-talk"><Image src="/images/talk.png" alt="Talk" width={100} height={100} /></a>
+                  <Image src="/images/arrow.png" alt="Arrow" width={80} height={80} />
+                  <a href="#" onClick={switchIndex} data-key="index-discussion"><div>
+                    <Image src="/images/discussion.png" alt="Discussion" width={80} height={80} />
+                    <Image src="/images/pc.png" alt="PC" width={80} height={80} />
+                  </div></a>
+                  <Image src="/images/arrow.png" alt="Arrow" width={80} height={80} />
+                  <a href="#" onClick={switchIndex} data-key="index-happy"><Image src="/images/happy.png" alt="Happy" width={100} height={100} /></a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
