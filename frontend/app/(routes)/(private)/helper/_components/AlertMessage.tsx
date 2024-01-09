6cbf89ff4c1ed6ba716e255bba42484579ef6f7f
zio-tt <zio.tt.dev@gmail.com>
2024-01-09 14:35:48 +0900

    WIP: helperのcomponentとtypesを追跡対象に追加

---
+++ frontend/app/(routes)/(private)/helper/_components/AlertMessage.tsx
@@ -0,0 +1,17 @@
+// types
+import { Error } from '../_types/Error';
+
+interface AlertMessageProps {
+  errors: Error[];
+}
+
+const AlertMessage = ({ errors }: AlertMessageProps) => {
+  return errors.map((error, index) => (
+    <div key={index} role="alert" className="flex alert alert-error w-[80%] mb-[3vh]">
+      <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+      <span>{error.message}</span>
+    </div>
+  ));
+}
+
+export { AlertMessage }
---
+++ frontend/app/(routes)/(private)/helper/_components/UserInterface/UserInterface.tsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+
+interface UserInterfaceProps {
+  tags: string[];
+  setTags: (tags: string[]) => void;
+  isPublic: boolean;
+  setIsPublic: (isPublic: boolean) => void;
+  saveDecision: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+const UserInterface = ({ tags, setTags, isPublic, setIsPublic, saveDecision }: UserInterfaceProps) => {
+  const [ inputTags, setInputTags ] = useState<string>('');
+  const [ labelBgColor, setLabelBgColor ] = useState<string>('bg-blue-100');
+  const [ labelTextColor, setLabelTextColor ] = useState<string>('text-blue-800');
+  const [ alert, setAlert ] = useState<string[]>([]);
+  const [ isTagAlert, setIsTagAlert ] = useState<boolean>(false);
+
+  const handleTagChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputTags(e.target.value);
+    const inputTags = e.target.value.split(',').map(tag => tag.slice(0, 10).trim());
+    // タグの個数が6個以下、各タグの文字数が10文字以下であることを確認
+    const newTags = inputTags.slice(0, 6);
+    setTags(newTags);
+  };
+
+  const handlePublicChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsPublic(e.target.checked);
+  };
+
+  useEffect(() => {
+    if (inputTags.split(',').length > 6) {
+      const alertMessages = [...alert, 'タグの個数が6個を超えています。']
+      setLabelBgColor('bg-red-400');
+      setLabelTextColor('text-red-800');
+      setAlert(alertMessages);
+      setIsTagAlert(true);
+    } else if (inputTags.split(',').some(tag => tag.length > 10)) {
+      const alertMessages = [...alert, 'タグの文字数が10文字を超えています。']
+      setLabelBgColor('bg-red-400');
+      setLabelTextColor('text-red-800');
+      setAlert(alertMessages);
+      setIsTagAlert(true);
+    } else {
+      setLabelBgColor('bg-blue-100');
+      setLabelTextColor('text-blue-800');
+      setAlert([]);
+      setIsTagAlert(false);
+    }
+  }, [inputTags]);
+
+  return (
+    <div className='w-full h-full flex flex-col justify-end space-y-4'>
+      {/* タグ表示領域 */}
+      <div className='w-[70%] mx-auto my-4 bg-gray-100 p-2 rounded-lg flex-grow overflow-auto'>
+        {tags.map((tag, index) => (
+          <div key={index} className={`text-xs font-semibold mb-2 px-2.5 py-1 rounded ${labelTextColor} ${labelBgColor}`}>
+            {tag}
+          </div>
+        ))}
+      </div>
+      { isTagAlert && (
+        <div className='w-[70%] mx-auto text-red-600'>{alert}</div>
+      )}
+      {/* タグ入力 */}
+      <div className='w-[70%] mx-auto'>
+        <label className="label">
+          <span className="label-text text-md text-black">カンマ区切りでタグを入力</span>
+        </label>
+        <input type="text"
+               id="tags"
+               className="bg-gray-50 border border-gray-300 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+               placeholder="最大6個、各10文字まで"
+               value={inputTags}
+               onChange={handleTagChange} />
+      </div>
+
+      {/* 保存ボタンと公開設定 */}
+      <div className='w-[70%] mx-auto flex flex-col items-end space-y-2 mb-4'>
+        <button className='btn btn-lg w-full text-2xl bg-white text-black hover:text-white' onClick={saveDecision}>保存する</button>
+        <label className="label cursor-pointer space-x-2">
+          <span className="label-text text-md text-black">全体に公開する</span>
+          <input type="checkbox" className="checkbox checkbox-primary" checked={isPublic} onChange={handlePublicChange} />
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export { UserInterface };
