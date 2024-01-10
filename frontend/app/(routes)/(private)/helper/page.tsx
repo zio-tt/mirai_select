0d2f6df1851f41043ae5bbd1f7f862f5835d57e3
zio-tt <zio.tt.dev@gmail.com>
2024-01-10 23:13:44 +0900

    Refecter: errorのリファクタリング

--- frontend/app/(routes)/(private)/helper/page.tsx
+++ frontend/app/(routes)/(private)/helper/page.tsx
@@ -1,68 +1,72 @@
 'use client';
 
-import  './style.css';
+import './style.css';
 import axios from 'axios';
 import type { MouseEvent } from 'react';
 
 // Components
-import { AlertMessage }     from './_components/AlertMessage';
-import { CharacterDisplay } from './_components/Character/CharacterDisplay';
-import { CheckModal }       from './_components/CheckModal';
-import { InputForm }        from './_components/UserInterface/InputForm';
-import { UserInterface }    from './_components/UserInterface/UserInterface';
-import { Loading }          from '@/app/_components/layouts/loading/layout';
+import { AlertMessage }       from './_components/AlertMessage';
+import { CharacterDisplay }   from './_components/Character/CharacterDisplay';
+import { CheckModal }         from './_components/CheckModal';
+import { InputForm }          from './_components/UserInterface/InputForm';
+import { UserInterface }      from './_components/UserInterface/UserInterface';
+import { Loading }            from '@/app/_components/layouts/loading/layout';
+import { useErrorHandling }   from './_hooks/useErrorHandling';
 // Hooks
 import { useState, useEffect } from 'react';
-import { useHelper } from '@/app/_contexts/HelperContext';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useHelper }           from '@/app/_contexts/HelperContext';
+import { useSession }          from 'next-auth/react';
 // Types
 import { Error } from './_types/Error';
-import { User, Character, Decision, Conversation } from '@/app/_types';
-import { set } from 'zod';
+import { Character, Conversation } from '@/app/_types';
 
 interface CharacterProps extends Character {
   avatar: string;
 }
 
-
 export default function decisionHelper () {
+  const { data: session, status } = useSession();
   // ページ読み込み時に取得する情報（キャラクター情報）
   const { characterData , setCharacterData } = useHelper();
   const { userData, setUserData } = useHelper();
+  // ドロワーとの連携
   const { isDrawerClick, setIsDrawerClick } = useHelper();
-  const [ isCharacterData, setIsCharacterData ] = useState<boolean>(false);
-  // 通信中の場合Loadingコンポーネントを表示する
+  // 深掘り機能
+  const [ beforeQueryText, setBeforeQueryText ] = useState('');
+  const [ conversationCount, setConversationCount ] = useState<number>(1);
+  // バックグラウンド機能（ローディング）
   const [ isLoading , setIsLoading ] = useState<boolean>(false);
   // エラー処理
-  const [ errors, setErrors ] = useState<Error[]>([]);
-  const [ isError, setIsError ] = useState<boolean>(false);
-  // 相談関係
+  const { errors, isError, addErrorMessages, removeErrorMessages, resetErrorMessages } = useErrorHandling();
+  // データ格納用
   const [ responses, setResponses ] = useState<string[]>([]);
   const [ decision, setDecision ] = useState<number>(0);
   const [ conversation, setConversation ] = useState<Conversation[]>([]);
-  const [ beforeConsultation, setBeforeConsultation ] = useState<string>('');
-  const [ placeholder, setPlaceholder ] = useState<string>('悩みを入力してください（50文字以内）');
   const [ isResponse, setIsResponse ] = useState<boolean>(false);
   // チェックボックス関係
-  // チェックボックスにチェックが入っている場合、確認用モーダルを表示しない
   const [ isCheckModal, setIsCheckModal ] = useState<boolean>(false);
 
   // ユーザーインターフェース関係
   const [ tags, setTags ] = useState<string[]>([]);
   const [ userDecision, setUserDecision ] = useState<number>(0);
   const [ isPublic, setIsPublic ] = useState<boolean>(false);
-
-  const { data: session, status } = useSession();
-
+  // 入力フォーム
   const { inputText, setInputText } = useHelper();
+  const [ placeholder, setPlaceholder ] = useState<string>('悩みを入力してください（50文字以内）');
   const { remainingTokens, setRemainingTokens } = useHelper();
-  const token = session?.appAccessToken;
 
-  const router = useRouter();
+  const token  = session?.appAccessToken;
 
-  // 非同期でバックエンドからユーザー情報を取得する関数
-  const fetchUserData = async () => {
+  {/* ページ読み込み時に使用する関数 */}
+  // ページ読み込み時にバックエンドからユーザー情報を取得する
+  useEffect(() => {
+    // ページ読み込みでエラー情報をリセットする
+    resetErrorMessages();
+    // ページ読み込み時にRailsAPIから必要な情報を取得する
+    fetchInitData();
+  }, []);
+
+  const fetchInitData = async () => {
     try {
       const response = await axios({
         method: 'post',
@@ -73,31 +77,20 @@ export default function decisionHelper () {
         },
         withCredentials: true,
       });
-      setUserData(response.data.user);
-      setCharacterData(response.data.characters);
+      // responseが正常に取得できた時
+      if (response.status === 200) {
+        setUserData(response.data.user);
+        setCharacterData(response.data.characters);
+      }
     } catch (error) {
-      addErrorMessage({
+      addErrorMessages({
         message: 'ユーザー情報の取得に失敗しました。',
         kind: 'fetch'
       });
-    } finally {
-      // 成功でも失敗でも実行される処理
-      setIsCharacterData(true);
     }
   }
 
-  const parseResponse = (data: any) => {
-    try {
-      const parsed = JSON.parse(data.choices[0].message.content);
-      setResponses([parsed.character1.character1_response, parsed.character2.character2_response ]);
-    } catch (e) {
-      addErrorMessage({
-        message: 'レスポンスデータの解析中にエラーが発生しました。',
-        kind: 'parse'
-      });
-    }
-  }
-
+  // Conversationの作成で必要なComponent
   const sendText = async (event: MouseEvent<HTMLElement>) => {
     event.preventDefault();
 
@@ -123,24 +116,43 @@ export default function decisionHelper () {
         data: { inputText },
         withCredentials: true,
       });
-      parseResponse(userCreateResponse.data.response);
-      setUserData(userCreateResponse.data.user);
-      setDecision(userCreateResponse.data.decision);
-      setConversation(userCreateResponse.data.conversation);
+      if (userCreateResponse.status == 200) {
+        parseResponse(userCreateResponse.data.response);
 
-      console.log("userCreateResponse:",userCreateResponse.data);
+        // レスポンスデータを各Stateに格納
+        setUserData(userCreateResponse.data.user);
+        setDecision(userCreateResponse.data.decision);
+        setConversation(userCreateResponse.data.conversation);
+        // 1,2度目のConversationで共通のState
+        setIsLoading(false);
+        setBeforeQueryText(inputText);
 
+        if (!isResponse) {
+          setIsResponse(true);
+          setPlaceholder('悩みを深掘りする場合は追加の相談文を入力してください（50文字以内）');
+          setInputText('');
+          setConversationCount(2);
+        } else {
+          setConversationCount(3);
+        }
+      }
     } catch (error: any) {
-      addErrorMessage({
+      addErrorMessages({
         message: error.message,
         kind: 'openai'
       });
-    } finally {
-      setBeforeConsultation(inputText);
-      setIsResponse(true);
-      setIsLoading(false);
-      setPlaceholder('悩みを深掘りする場合は追加の相談文を入力してください（50文字以内）');
-      setInputText('');
+    }
+  }
+
+  const parseResponse = (data: any) => {
+    try {
+      const parsed = JSON.parse(data.choices[0].message.content);
+      setResponses([parsed.character1.character1_response, parsed.character2.character2_response ]);
+    } catch (e) {
+      addErrorMessages({
+        message: 'レスポンスデータの解析中にエラーが発生しました。',
+        kind: 'parse'
+      });
     }
   }
 
@@ -154,7 +166,7 @@ export default function decisionHelper () {
     // タグが少なくとも1つ存在し、ユーザーがキャラクターを選択しているかを確認
     if (tags.length === 0 || userDecision === 0) {
       // エラーメッセージを表示
-      addErrorMessage({
+      addErrorMessages({
         message: '最低1つのタグを入力し、どちらかのキャラクターを選択してください。',
         kind: 'validation'
       });
@@ -181,7 +193,7 @@ export default function decisionHelper () {
       });
       console.log(response.data.message);
     } catch (error: any) {
-      addErrorMessage({
+      addErrorMessages({
         message: error.message,
         kind: 'openai'
       });
@@ -191,36 +203,32 @@ export default function decisionHelper () {
     }
   }
 
+  // すべてのstateを初期化する関数
   const initializeState = () => {
+    setResponses([]);
+    setBeforeQueryText('');
+    setPlaceholder('悩みを入力してください（50文字以内）');
+    setIsResponse(false);
+    setInputText('');
+    setIsLoading(false);
     setTags([]);
     setUserDecision(0);
     setIsPublic(false);
-    setIsResponse(false);
-    setIsLoading(false);
-    setPlaceholder('悩みを入力してください（50文字以内）');
-    setInputText('');
-    setResponses([]);
     setDecision(0);
-    setBeforeConsultation('');
     setConversation([]);
-    setIsDrawerClick(false);
+    setConversationCount(1);
+    setRemainingTokens(userData!.token);
 
     return;
   }
 
-  // ページ読み込み時にバックエンドからユーザー情報を取得する
-  useEffect(() => {
-    setIsError(false);
-    setErrors([]);
-    fetchUserData();
-  }, []);
 
   useEffect(() => {
     console.log(isDrawerClick)
     if(isDrawerClick) {
       // ページ内のすべてのstateを初期化
       setResponses([]);
-      setBeforeConsultation('');
+      setBeforeQueryText('');
       setPlaceholder('悩みを入力してください（50文字以内）');
       setIsResponse(false);
       setInputText('');
@@ -229,68 +237,44 @@ export default function decisionHelper () {
     }
   }, [isDrawerClick]);
 
-  // エラーメッセージをエラー配列に追加する関数
-  const addErrorMessage = ({ message, kind } : Error) => {
-    // 既に同じ種類のエラーが存在する場合、エラーを上書きする
-    const error = errors.find(error => error.kind === kind);
-    if (error) {
-      error.message = message;
-      return;
-    }
-    setErrors([...errors, {message: message, kind: kind}]);
-    setIsError(true);
-  }
-
-  // エラーメッセージをエラー配列から削除する関数
-  const removeErrorMessage = (kind: string) => {
-    const newErrors = errors.filter(error => error.kind !== kind);
-    setErrors(newErrors);
-    if (newErrors.length === 0) {
-      setIsError(false);
-    }
-  }
 
   useEffect(() => {
     if(tags.length > 0 && userDecision !== 0) {
-      removeErrorMessage('validation');
+      removeErrorMessages('validation');
     }
   }, [tags, userDecision]);
 
   // 入力フォームの内容が変更された場合の処理
   useEffect(() => {
     const inputTextLength = inputText.length;
-    const remainingToken = userData ? userData.token - inputTextLength: null;
-
-    if (inputTextLength === 0) {
-      setErrors([]);
-      setIsError(false);
-    }
-
-    if (userData) {
-      setRemainingTokens(userData.token - inputText.length);
-    }
-
-    // 入力テキストが所持トークンを超える場合
-    if (userData && remainingToken! < 0) {
-      addErrorMessage({
-        message: `トークン数が不足しています。（残りトークン数：${remainingToken}）`,
-        kind: 'token'
-      });
-    } else if (userData && remainingToken! >= 0) {
-      removeErrorMessage('token');
-    }
+    // 入力フォームに何も入力されていない場合エラーをリセットする
+    if (inputTextLength === 0) {resetErrorMessages()}
+    // 入力フォームに入力された文字数をトークン数から引いて残りトークン数を計算する
+    if (userData) {setRemainingTokens(userData.token - inputText.length)}
 
     // 入力テキストが50文字を超える場合
     if (inputTextLength > 50) {
-      addErrorMessage({
+      console.log("文字数がオーバーしています。")
+      addErrorMessages({
         message: `文字数がオーバーしています。（現在の入力文字数：${inputTextLength}/50文字）`,
         kind: 'inputText'
       });
     } else if (inputTextLength <= 50) {
-      removeErrorMessage('inputText');
+      removeErrorMessages('inputText');
     }
   }, [inputText, userData]);
 
+  // 残りトークン数が0以下になった場合
+  useEffect(() => {
+    if (remainingTokens < 0) {
+      addErrorMessages({
+        message: `トークン数が不足しています。（残りトークン数：${remainingTokens}）`,
+        kind: 'token'
+      });
+    }
+  }, [remainingTokens])
+
+  // エラーがなくなった時にスクロールをトップに戻す
   useEffect(() => {
     if(!isError) {
       window.scrollTo(0, 0);
@@ -299,9 +283,9 @@ export default function decisionHelper () {
 
   return (
     <>
-      { !isCharacterData && <div className='w-full min-h-screen'><Loading /></div> }
+      { !characterData && <div className='w-full min-h-screen'><Loading /></div> }
       { isLoading && <div className='w-full min-h-screen'><Loading /></div> }
-      { isCharacterData && !isLoading && (
+      { characterData && !isLoading && (
         <div className='flex flex-col w-full min-h-screen items-center justify-center py-[3vh]'>
         { isError && <AlertMessage errors={errors} />}
           <div id='main-contents' className='w-[70vw] h-[90vh] bg-gray-200/30 rounded-md border-2 border-black shadow-lg flex flex-col items-center justify-center p-3 overflow-hidden'>
@@ -310,7 +294,7 @@ export default function decisionHelper () {
                 { isResponse && (
                   <>
                     <div className='flex w-[90%] h-[10%] border-2 rounded-md border-black bg-white mx-6 mt-6 mb-2 items-center justify-center text-black'>
-                      <div className='flex'>相談内容：{beforeConsultation}</div>
+                      <div className='flex'>相談内容：{beforeQueryText}</div>
                     </div>
                     <div className='flex w-[60%] h-[10%] rounded-xl bg-pink-200 items-center justify-center text-black'>
                       良いと思った方の意見を選択してください。
@@ -336,12 +320,13 @@ export default function decisionHelper () {
               </div>
             </div>
             <div id='input-form' className='w-full grow-[3] flex items-center justify-center border-2 rounded-md border-black mt-3'>
-              <InputForm
+              { conversationCount != 3 && <InputForm
                 inputText={inputText}
                 handleChange={handleChange}
                 setInputText={setInputText}
                 placeholder={placeholder}
                 sendText={sendText} />
+              }
             </div>
           </div>
         </div>
