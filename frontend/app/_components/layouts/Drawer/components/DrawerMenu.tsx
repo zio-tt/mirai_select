553c1c4db4cbba2c50ab05eb262a9d223421b8f8
zio-tt <zio.tt.dev@gmail.com>
2024-01-12 01:57:50 +0900

    Fix: 本番環境でのみindexページが読み込まれないエラーの対応

--- frontend/app/_components/layouts/Drawer/components/DrawerMenu.tsx
+++ frontend/app/_components/layouts/Drawer/components/DrawerMenu.tsx
@@ -30,7 +30,7 @@ const DrawerMenu = ({ url, text, imageURL, onClick }: DrawerMenuProps) => {
         <Image src={imageURL} alt={text} width={100} height={100} className='flex object-cover' />
       </div>
       <div className='w-[80%] flex h-[10vh] p-1 border-black border-2 rounded-lg items-center justify-center hover:bg-[#ffcf82]'>
-        <Link href={url} onClick={onClick} prefetch={true} className='text-[0.9rem] font-bold leading-5 mr-3 ml-3 text-center'>{textElements}</Link>
+        <Link href={url} onClick={onClick} prefetch={false} className='text-[0.9rem] font-bold leading-5 mr-3 ml-3 text-center'>{textElements}</Link>
       </div>
     </div>
   );
