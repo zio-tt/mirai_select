a777662f133928df04c1aebdfbc852f99ab9a24b
zio-tt <zio.tt.dev@gmail.com>
2024-02-14 01:57:11 +0900

    Fix: OGPがうまくいったので元に戻した

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import AppLayout from './_components/layouts/AppLayout'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://mirai-select.net/'),
   title: 'ミライセレクト | あなたの選択を助けるアプリ',
   description:
     'ミライセレクトはあなたの選択を助けるアプリです。AIがあなたの悩み事に対して2つの選択肢を提示します。',
@@ -11,7 +12,6 @@ export const metadata: Metadata = {
     title: 'ミライセレクト | あなたの選択を助けるアプリ',
     description:
       'ミライセレクトはあなたの選択を助けるアプリです。AIがあなたの悩み事に対して2つの選択肢を提示します。',
-    url: 'https://mirai-select.net/',
   },
   twitter: {
     title: 'ミライセレクト | あなたの選択を助けるアプリ',
