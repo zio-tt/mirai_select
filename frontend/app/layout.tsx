c22c20eef238d78022ca9394725f447e46e6ce90
zio-tt <zio.tt.dev@gmail.com>
2024-02-14 01:54:20 +0900

    Fix: URLが保管されてしまう問題

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,7 +4,6 @@ import AppLayout from './_components/layouts/AppLayout'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://mirai-select.net/'),
   title: 'ミライセレクト | あなたの選択を助けるアプリ',
   description:
     'ミライセレクトはあなたの選択を助けるアプリです。AIがあなたの悩み事に対して2つの選択肢を提示します。',
@@ -12,6 +11,7 @@ export const metadata: Metadata = {
     title: 'ミライセレクト | あなたの選択を助けるアプリ',
     description:
       'ミライセレクトはあなたの選択を助けるアプリです。AIがあなたの悩み事に対して2つの選択肢を提示します。',
+    url: 'https://mirai-select.net/',
   },
   twitter: {
     title: 'ミライセレクト | あなたの選択を助けるアプリ',
